2371165c0bc7616cac0f02e38254a7734362737f
Shaoshuai Dai <shdai@cisco.com>
2021-01-27 00:27:27 +0800

    tcp monitoring agent

--- main.go
+++ main.go
@@ -1,40 +1,94 @@
 package main
 
 import (
-	"bufio"
+	"bytes"
+	"encoding/gob"
 	"fmt"
+	"io"
 	"log"
 	"net"
+	"time"
 )
 
-func main() {
-	addr, err := net.ResolveTCPAddr("tcp", ":32412")
-	if err != nil {
-		panic(err)
-	}
-	l, err := net.ListenTCP("tcp", addr)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer l.Close()
-	for {
+// Create your custom data struct
+type Message struct {
+	ID   string
+	Data string
+}
 
-		conn, err := l.Accept()
-		if err != nil {
-			log.Fatal(err)
+func logerr(err error) bool {
+	if err != nil {
+		if netErr, ok := err.(net.Error); ok && netErr.Timeout() {
+			log.Println("read timeout:", err)
+		} else if err == io.EOF {
+		} else {
+			log.Println("read error:", err)
+		}
+		return true
+	}
+	return false
+}
+
+func read(conn net.Conn) {
+	// create a temp buffer
+	tmp := make([]byte, 500)
+
+	// loop through the connection to read incoming connections. If you're doing by
+	// directional, you might want to make this into a seperate go routine
+	for {
+		_, err := conn.Read(tmp)
+		if logerr(err) {
+			break
 		}
 
-		go func(c net.Conn) {
-			for {
-				message, err := bufio.NewReader(conn).ReadString('\n')
-				if err != nil {
-					log.Printf("Error: %+v", err.Error())
-					return
-				}
+		// convert bytes into Buffer (which implements io.Reader/io.Writer)
+		tmpbuff := bytes.NewBuffer(tmp)
+		tmpstruct := new(Message)
 
-				log.Println("Message:", string(message))
-				fmt.Println(string(message))
-			}
-		}(conn)
+		// creates a decoder object
+		gobobj := gob.NewDecoder(tmpbuff)
+		// decodes buffer and unmarshals it into a Message struct
+		gobobj.Decode(tmpstruct)
+
+		// lets print out!
+		fmt.Println(tmpstruct)
+		return
+	}
+}
+
+func resp(conn net.Conn) {
+	msg := Message{ID: "Yo", Data: "Hello back"}
+	bin_buf := new(bytes.Buffer)
+
+	// create a encoder object
+	gobobje := gob.NewEncoder(bin_buf)
+	// encode buffer and marshal it into a gob object
+	gobobje.Encode(msg)
+
+	conn.Write(bin_buf.Bytes())
+	conn.Close()
+}
+
+func handle(conn net.Conn) {
+	timeoutDuration := 2 * time.Second
+	fmt.Println("Launching server...")
+	conn.SetReadDeadline(time.Now().Add(timeoutDuration))
+
+	remoteAddr := conn.RemoteAddr().String()
+	fmt.Println("Client connected from " + remoteAddr)
+
+	read(conn)
+	resp(conn)
+}
+
+func main() {
+	server, _ := net.Listen("tcp", ":8081")
+	for {
+		conn, err := server.Accept()
+		if err != nil {
+			log.Println("Connection error: ", err)
+			return
+		}
+		go handle(conn)
 	}
 }
